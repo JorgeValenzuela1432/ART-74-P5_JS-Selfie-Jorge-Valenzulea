059d751127a2b97b475e52230d9e3480d0c8994a
JorgeValenzuela1432 <jorge.valenzuela@sjsu.edu>
2024-11-06 12:25:41 -0800

    Selfie P5_JS project for ART-74

---
+++ ART_74_Selfie_Jorge_Valenzuela.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+  
+ background(220,215,255);
+ 
+ 
+}
+
+
+function draw() {
+  ellipse(250, 250, 180, 210);
+  rect(180, 220, 55, 40);
+  rect(260, 220, 55, 40);
+  line(260, 235, 235, 235);
+  ellipse(250, 555, 200, 400);
+  
+}
